567a84d5e404fcc0601c71a84ba6d558d585994d
Agamand <Basset.cyril@gmail.com>
2013-07-19 20:33:59 +0200

    Implement : ShadowMapRenderer

---
+++ src/Agmd3D/Core/ShadowMapRenderer.cpp
@@ -0,0 +1,138 @@
+#include <Core/ShadowMapRenderer.h>
+#include <Core/Renderer.h>
+#include <Core/Tools/Fast2DSurface.h>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+namespace Agmd
+{
+    mat4 ShadowMapRenderer::bias = mat4(
+                                        0.5, 0.0, 0.0, 0.0,
+                                        0.0, 0.5, 0.0, 0.0,
+                                        0.0, 0.0, 0.5, 0.0,
+                                        0.5, 0.5, 0.5, 1.0
+                                        );
+
+    mat4 ShadowMapRenderer::s_lookupMatrix[] = {
+    lookAt(vec3(0,0,0),vec3(1,0,0),vec3(0,0,1)),
+    lookAt(vec3(0,0,0),vec3(-1,0,0),vec3(0,0,1)),
+    lookAt(vec3(0,0,0),vec3(0,1,0),vec3(0,0,1)),
+    lookAt(vec3(0,0,0),vec3(0,-1,0),vec3(0,0,1)),
+    lookAt(vec3(0,0,0),vec3(0,0,1),vec3(-1,0,0)),
+    lookAt(vec3(0,0,0),vec3(0,0,-1),vec3(1,0,0))};
+    ShadowMapRenderer::ShadowMapRenderer(const ivec2& size) :
+    m_currentDepth(0),
+    m_shadowSize(size),
+    m_currentLight(NULL),
+    m_offset(0.05f)
+    {
+        for(int i = 0; i < 6; i++)
+            m_depth[i].Create(m_shadowSize,PXF_DEPTH,TEXTURE_2D,TEX_NOMIPMAP);
+        m_framebuffer = Renderer::Get().CreateFrameBuffer();
+        RenderBuffer* color_test = Renderer::Get().CreateRenderBuffer(m_shadowSize,PXF_R16G16B16);
+        m_framebuffer->SetTexture(m_depth[0],DEPTH_ATTACHMENT);
+        m_framebuffer->SetRender(color_test,COLOR_ATTACHMENT);
+        //m_framebuffer->DrawBuffer(0);
+        m_shadowcast.LoadFromFile("Shader/RenderingShader/ShadowCastShader.glsl");
+    }
+
+    ShadowMapRenderer::~ShadowMapRenderer()
+    {
+        delete m_framebuffer;
+    }
+
+    void ShadowMapRenderer::Reset()
+    {
+        m_framebuffer->Clear(CLEAR_DEPTH);
+    }
+
+    void ShadowMapRenderer::BeginLight(Light* l)
+    {
+        Renderer::Get().SetCurrentProgram(m_shadowcast.GetShaderProgram());
+        m_currentLightType = l->GetType();
+        if(m_currentDepth)
+            SetDepth(0);
+        m_framebuffer->Bind();
+        Renderer::Get().SetViewPort(ivec2(0),ivec2(m_shadowSize));
+        if(m_currentLightType == LightType::LIGHT_DIR)
+        {
+            mat4 proj = ortho<float>(-1000,1000,-1000,1000,-100,5000);
+            m_shadow_matrix[0] = proj*lookAt(l->GetDirection()*1000.0f,vec3(0),vec3(0,0,1));
+        
+
+        }else if(m_currentLightType == LightType::LIGHT_POINT)
+        {
+            ivec2 screen = Renderer::Get().GetScreen();
+            float aspect = screen.x/((float)screen.y);
+            mat4 proj = perspective(90.0f,aspect,1.0f,l->GetRange());
+            m_shadow_matrix[0] = proj*lookAt(l->GetPosition(),vec3(1,0,0),vec3(0,0,1));
+
+        }else
+        {
+            ivec2 screen = Renderer::Get().GetScreen();
+            float aspect = screen.x/((float)screen.y);
+            mat4 proj = perspective(acos(l->GetOuterAngle())*180.0f/(float)M_PI*2.0f,aspect,1.0f,l->GetRange());
+            m_shadow_matrix[0] = proj*lookAt(l->GetPosition(),l->GetPosition()-l->GetDirection(),vec3(0,0,1));
+        }
+        Renderer::Get().GetCurrentProgram()->SetParameter("depthMVP",m_shadow_matrix[0]);
+        Renderer::Get().SetCullFace(1);
+    }
+
+
+    /*
+    ONLY for Omni directionnal light like point light
+    */
+
+    void ShadowMapRenderer::SetDepth(int face)
+    {
+        if(face < 0 || face > 5)
+            return;
+
+        m_currentDepth = face;
+        m_framebuffer->SetTexture(m_depth[face],DEPTH_ATTACHMENT);
+        m_framebuffer->Bind();
+        ivec2 screen = Renderer::Get().GetScreen();
+        float aspect = screen.x/((float)screen.y);
+        mat4 proj = perspective(90.0f,aspect,1.0f,m_currentLight->GetRange());
+    }
+
+    void ShadowMapRenderer::EndLight()
+    {
+        Renderer::Get().SetViewPort(ivec2(0),Renderer::Get().GetScreen());
+        Renderer::Get().SetCurrentProgram(NULL);
+        m_framebuffer->UnBind();
+        Renderer::Get().SetCullFace(2);
+    }
+
+    void ShadowMapRenderer::SetupForRendering()
+    {
+        
+        if(m_currentLightType == LightType::LIGHT_POINT)
+        {
+            mat4 VPB[6];
+            for(int i = 0; i < 6; i++)
+            {
+                VPB[i] = bias*m_shadow_matrix[i];
+                Renderer::Get().SetTexture(3+i,m_depth[i].GetTexture());
+            }
+            Renderer::Get().GetCurrentProgram()->SetParameter("depthVPBias",VPB,6);
+
+        }else
+        {
+            mat4 VPB = bias*m_shadow_matrix[0];
+            Renderer::Get().GetCurrentProgram()->SetParameter("depthVPBias",VPB);
+            Renderer::Get().SetTexture(3,m_depth[0].GetTexture());
+        }
+        Renderer::Get().GetCurrentProgram()->SetParameter("u_offset", m_offset);
+    }
+
+    void ShadowMapRenderer::SetOffset(float offset)
+    {
+        m_offset = offset;
+    }
+
+    Texture& ShadowMapRenderer::GetShadowDepth()
+    {
+        return m_depth[0];
+    }
+}
